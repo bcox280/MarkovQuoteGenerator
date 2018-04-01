0920766393d87cd94055e245f2090defe23818bc
bcox280 <blairacox@gmail.com>
2018-04-02 03:20:20 +1200

    Fix nested quotes not terminating correctly, ie 'Game of tennis Also fix termination on non fullstop finishes

--- MarkovQuoteGenerator/pyquote.py
+++ MarkovQuoteGenerator/pyquote.py
@@ -4,12 +4,18 @@ from itertools import islice
 import random
 
 transition = {}
+nested_transition = {}
 prior = {}
+nested_quotes = False
+finishable_quote = True
 
 
 def generate_next(current):
     """Generate the next word in the quote"""
-    return generate_init(transition[current])
+    if nested_quotes:
+        return generate_init(nested_transition[current])
+    else:
+        return generate_init(transition[current])
 
 
 def generate_init(dictionary):
@@ -33,36 +39,72 @@ def add_to_p(word):
 def add_in(s, t):
     """Add corresponding words into a dictionary of dictionaries"""
 
-    if s in transition:
-        inner_dict = transition[s]
-        if t in inner_dict:
-            inner_dict[t] = inner_dict[t] + 1
+    global nested_quotes
+
+    if s.startswith('\''):
+        nested_quotes = True
+
+    if s.endswith('\'') | s.endswith('\'.') | s.endswith('\'!') | s.endswith('\'?'):
+        nested_quotes = False
+
+    """If the current words are nested inside quotes"""
+    if nested_quotes:
+
+        if s in nested_transition:
+            inner_dict = nested_transition[s]
+            if t in inner_dict:
+                inner_dict[t] = inner_dict[t] + 1
+            else:
+                inner_dict[t] = 1
         else:
-            inner_dict[t] = 1
+            nested_transition[s] = {t: 1}
+
     else:
-        transition[s] = {t: 1}
+        if s in transition:
+            inner_dict = transition[s]
+            if t in inner_dict:
+                inner_dict[t] = inner_dict[t] + 1
+            else:
+                inner_dict[t] = 1
+        else:
+            transition[s] = {t: 1}
 
 
 def create_t():
+    global nested_quotes
     """Create the transition object."""
     inf = open("../SourceText/quotes.txt", "r")
     lines = (text.split() for text in inf)
 
     for line in lines:
+        nested_quotes = False
         first_word = line[0]
         add_to_p(first_word)
         for other_word in line[1:]:
             add_in(first_word, other_word)
             first_word = other_word
 
+    global finishable_quote
     quote = generate_init(prior)
     current_word = quote
 
     while True:
+
+        """Set flags if the words are currently nested"""
+        if current_word.startswith('\''):
+            finishable_quote = False
+            nested_quotes = True
+        if current_word.endswith('\'') | current_word.endswith('\'.') | current_word.endswith('\'!') | current_word.endswith('\'?'):
+            finishable_quote = True
+            nested_quotes = False
+
+        if (current_word.endswith('.') | current_word.endswith('?') | current_word.endswith('!')
+            | current_word.endswith('.\'') | current_word.endswith('?\'') | current_word.endswith(
+                    '!\'')) & finishable_quote:
+            break
+
         current_word = generate_next(current_word)
         quote = quote + " " + current_word
-        if current_word.endswith('.'):
-            break
 
     print(quote)
 
